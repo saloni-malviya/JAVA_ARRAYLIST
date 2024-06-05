05f01b05657bfffd3f732ff2a5c43b9dfac3c5b6
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-06-05 20:46:29 +0530

    Add files via upload

---
+++ ArrayList_example.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Arraylist_concept;
+import java.util.ArrayList;
+public class ArrayList_example {
+    public static void main(String[]args)
+    {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(20);
+        list.add(40);
+        System.out.println(list);
+    }
+}
