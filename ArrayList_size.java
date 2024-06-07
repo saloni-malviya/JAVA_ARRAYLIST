0e6d20e96ec0b8b02f7b7182caa71efa2076e59d
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-06-07 19:59:57 +0530

    Add files via upload

---
+++ ArrayList_size.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//......................ArrayList size..........................
+package Arraylist_concept;
+import java.util.ArrayList;
+public class ArrayList_size {
+    public static void main(String[]args)
+    {
+        ArrayList<Integer> l=new ArrayList<>();
+        l.add(10);
+        l.add(70);
+        l.add(40);
+        l.add(60);
+        l.add(30);
+        l.add(20);
+        System.out.println("The size of list is:"+l.size());
+    }
+}
